3a56701c342331cbc23230c8677444edf1cf451d
AFMaldonado <96585213+AFMaldonado@users.noreply.github.com>
2023-03-31 12:12:32 -0500

    Update creacion_db_tablas.sql

--- creacion_db_tablas.sql
+++ creacion_db_tablas.sql
@@ -1,9 +1,24 @@
 CREATE DATABASE `db_purchase`;
 
-CREATE TABLE `my_table` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `age` int DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `orders` (
+  `order_number` varchar(50) DEFAULT NULL,
+  `order_status` varchar(50) DEFAULT NULL,
+  `customer_email` varchar(255) DEFAULT NULL,
+  `preferred_delivery_date` datetime DEFAULT NULL,
+  `preferred_delivery_hours` varchar(50) DEFAULT NULL,
+  `sales_person` varchar(50) DEFAULT NULL,
+  `notes` text,
+  `address` varchar(255) DEFAULT NULL,
+  `neighbourhood` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `source` varchar(50) DEFAULT NULL,
+  `warehouse` varchar(50) DEFAULT NULL,
+  `shopify_id` decimal(18,0) DEFAULT NULL,
+  `sales_person_role` varchar(50) DEFAULT NULL,
+  `order_type` varchar(50) DEFAULT NULL,
+  `is_pitayas` varchar(2) DEFAULT NULL,
+  `discount_applications` text,
+  `payment_method` varchar(50) DEFAULT NULL
 );
+
